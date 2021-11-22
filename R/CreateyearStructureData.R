78fb684a190feca0a3b2461530e7ce3c083571fb
Kathryn Doering <kathryn.doering@noaa.gov>
2021-11-22 10:30:51 -0500

    make conversion script into a function

--- R/CreateyearStructureData.R
+++ R/CreateyearStructureData.R
@@ -1,102 +1,59 @@
-#............................................................
-#
-#     READ DATA FILE WITH SEASONS AS CONTINUOUS YEARS
-#     AND CONVERT INTO AN ANNUAL MODEL  
-#
-#AGURTZANE URTIZBEREA
-#............................................................
+#' read data file with seasons as continuous years and convert into an annual
+#' model
+#' @param dat The data file with seasons as continuous years
+#' @author Agurtzane Urtizberea, Kathryn Doering
+create_yr_structure_data <- function(dat) {
+  # datnew will be the dat file as an annual model
+  datnew <- dat
 
-library(r4ss)
-library(xlsx)
+  # MODIFY CATCHES ----
 
-wd <- "~/Max files for backup/Documents/Commitees/Global program/IOTC/WP Tropical tunas/2021/Reference_annual_modified_3areas_SEL_DW_updated/Data conversion"
+  help <- read.csv(file.path("inst","extdata", "helper.csv"))
 
-setwd(wd)
+  #Find the match between the years on the file (season as years)  and
+  # the real years.
+  pos <- match(datnew$catch$year[-1],help$ssyr)
+  summary(pos)
+  pos.na <- which(is.na(pos))
 
-dat <- SS_readdat("data.ss")
-datnew <- dat
+  datnew$catch[pos.na+1,]
 
-#.............................................
-#
-#         MODIFY CATCHES
-#
-#...........................................
+  #careful the first line is removed (999)
+  datnew$catch$seas[-1] <- help$ss[pos]
+  datnew$catch$year[-1] <- help$year[pos]
+  datnew$catch[pos.na+1,] <- dat$catch[pos.na+1,]
 
-help <- read.csv("~/Max files for backup/Documents/Commitees/Global program/IOTC/WP Tropical tunas/2021/Reference_annual_modified_3areas_SEL_DW_updated/Data conversion/helper.csv")
+  # MODIFY CPUE ----
+  pos <- match(datnew$CPUE$year,help$ssyr)
+  #summary(pos)
 
-head(help)
+  datnew$CPUE$seas <- (help$ss[pos]-1)*3+1
+  datnew$CPUE$year <- help$year[pos]
 
-#Find the match between the years on the file (season as years)  and 
-# the real years.
-pos <- match(datnew$catch$year[-1],help$ssyr)
-summary(pos)
-pos.na <- which(is.na(pos))
+  # MODIFY lencomp ----
+  pos <- match(dat$lencomp$Yr,help$ssyr)
 
-datnew$catch[pos.na+1,]
+  datnew$lencomp$Seas <- (help$ss[pos]-1)*3+1
+  datnew$lencomp$Yr <- help$year[pos]
 
-#careful the first line is removed (999)
-datnew$catch$seas[-1] <- help$ss[pos]
-datnew$catch$year[-1] <- help$year[pos]
-datnew$catch[pos.na+1,] <- dat$catch[pos.na+1,]
-write.table(datnew$catch,file="catch_annual.txt",row.names = FALSE,sep="\t")
+  # MODIFY tagging data ----
 
-#.............................................
-#
-#         MODIFY CPUE
-#
-#...........................................
+  #tag_release
+  pos <- match(dat$tag_releases$Yr,help$ssyr)
 
-pos <- match(datnew$CPUE$year,help$ssyr)
-summary(pos)
+  datnew$tag_releases$Season <- help$ss[pos]#(help$ss[pos]-1)*3+1
+  datnew$tag_releases$Yr <- help$year[pos]
+  datnew$tag_releases$Age <- floor(dat$tag_releases$Age/4)
+  pos <- match(dat$tag_releases$tfill,help$ssyr)
+  datnew$tag_releases$tfill <- help$year[pos]
 
-datnew$CPUE$seas <- (help$ss[pos]-1)*3+1
-datnew$CPUE$year <- help$year[pos]
+  #tag_recaps
+  pos <- match(dat$tag_recaps$Yr,help$ssyr)
 
-write.table(datnew$CPUE,file="CPUE_annual.txt",row.names = FALSE,sep="\t")
+  datnew$tag_recaps$Season <- help$ss[pos]#(help$ss[pos]-1)*3+1
+  datnew$tag_recaps$Yr <- help$year[pos]
 
-
-#.............................................
-#
-#         MODIFY lencomp
-#
-#...........................................
-
-head(datnew$lencomp)
-summary(datnew$lencomp$Yr)
-
-pos <- match(dat$lencomp$Yr,help$ssyr)
-summary(pos)
-
-datnew$lencomp$Seas <- (help$ss[pos]-1)*3+1
-datnew$lencomp$Yr <- help$year[pos]
-write.table(datnew$lencomp,file="lencomp_annual.txt",row.names = FALSE,sep="\t")
-
-
-#.............................................
-#
-#         MODIFY taggind data
-#
-#...........................................
-
-#tag_release
-head(datnew$tag_releases)
-pos <- match(dat$tag_releases$Yr,help$ssyr)
-summary(pos)
-
-datnew$tag_releases$Season <- help$ss[pos]#(help$ss[pos]-1)*3+1
-datnew$tag_releases$Yr <- help$year[pos]
-datnew$tag_releases$Age <- floor(dat$tag_releases$Age/4)
-pos <- match(dat$tag_releases$tfill,help$ssyr)
-datnew$tag_releases$tfill <- help$year[pos]
-write.table(datnew$tag_releases ,file="tag_release_annual.txt",row.names = FALSE,sep="\t")
-
-#tag_recaps
-head(datnew$tag_recaps)
-pos <- match(dat$tag_recaps$Yr,help$ssyr)
-summary(pos)
-
-datnew$tag_recaps$Season <- help$ss[pos]#(help$ss[pos]-1)*3+1
-datnew$tag_recaps$Yr <- help$year[pos]
-write.table(datnew$tag_recaps,file="tag_recaps_annual.txt",row.names = FALSE,sep="\t")
-
-SS_writedat_3.30(datnew,outfile="update3_annual.dat")
+  # may want to write this to a file
+  #SS_writedat_3.30(datnew,outfile="update3_annual.dat")
+  datnew
+}
