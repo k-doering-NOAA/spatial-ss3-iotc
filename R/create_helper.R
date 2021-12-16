1646e281d7b626d5ddbb2b7126f624c2545e53cf
Kathryn Doering <kathryn.doering@noaa.gov>
2021-12-16 11:49:07 -0500

    add missing yrs to the helper seas to yrs converter
    missing years 1 - 12

---
+++ R/create_helper.R
@@ -0,0 +1,11 @@
+# create helper.csv
+
+# ssyr <- 1:328
+# yrc <- seq(1947.125, 2028.875, by = 0.25)
+# year <- floor(yrc)# the integer year
+# ss <- rep(1:4, length.out = length(ssyr))# the season in ss3
+#
+# helper <- data.frame(ssyr = ssyr, yrc = yrc, year = year, ss = ss)
+#
+# write.csv(helper, file = "inst/extdata/helper.csv", row.names = FALSE)
+
